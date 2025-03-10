1285027734db769822acf75719114575579096eb
Wilson Cheung <info@wilsoncheung.me>
2025-03-10 10:51:36 +0000

    Another temporary commit

--- acubed/foo.py
+++ acubed/foo.py
@@ -14,14 +14,11 @@ def foo(bar: str) -> str:
 
 def foozeball(bar: str) -> str:
     """Summary line.
-
-    Extended description of function.
-
-    Args:
-        bar: Description of input argument.
-
-    Returns:
-        Description of return value
+asfkhaskjfsahkjfask
+ashfkjahsfkjahskfas
+sajfkashfkjashfkjashf
+haskfjhaskjfhsakjfas
+hajfshakjfhsakf
     """
     print("Hello Worlds!")
     return bar
